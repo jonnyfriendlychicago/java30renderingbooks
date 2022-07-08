9b65344eafaf850e079de6f6d582afa55a0260a4
Jon Friend <62109917+jonnyfriendlychicago@users.noreply.github.com>
2022-07-08 10:20:57 -0500

    first commit - template java app running

---
+++ src/main/java/com/jonfriend/java30renderingbooks/Java30renderingbooksApplication.java
@@ -0,0 +1,13 @@
+package com.jonfriend.java30renderingbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java30renderingbooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java30renderingbooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jonfriend/java30renderingbooks/Java30renderingbooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonfriend.java30renderingbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java30renderingbooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
